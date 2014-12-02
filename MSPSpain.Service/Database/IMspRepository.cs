ab3900e4ac62d4d6a8428aeb1004dba23cf48fcb
Daniel Rozo <drozo@vistaprint.com>
2014-12-02 09:48:30 +0100

    WIP

---
+++ MSPSpain.Service/Database/IMspRepository.cs
@@ -0,0 +1,14 @@
+﻿using MSPSpain.Service.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSPSpain.Service.Database
+{
+    public interface IMspRepository
+    {
+        Task<IList<Msp>> GetMsps();
+    }
+}
---
+++ MSPSpain.Service/Model/Msp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSPSpain.Service.Model
+{
+    public class Msp
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastNames { get; set; }
+        public string City { get; set; }
+        public string AutonomousCommunity { get; set; }
+    }
+}
