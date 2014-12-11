24852fa2dd5a5dde29df94108a5d429be6e29920
CKGrafico <quique@ckgrafico.com>
2014-12-11 15:22:29 +0100

    Add Nunit test framework

--- MSPSpain.Tests/Frontend.cs
+++ MSPSpain.Tests/Frontend.cs
@@ -1,5 +1,17 @@
 ﻿using System;
+#if NUNIT
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
+#else
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+using NUnitAssert = NUnit.Framework.Assert;
+using MsAssert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 using Newtonsoft.Json.Schema;
 using Newtonsoft.Json.Linq;
 using System.IO;
