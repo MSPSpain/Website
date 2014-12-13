19571d12d390e5a2a6a50922e07a596eb85290f4
Quique Fdez Guerra <quique@ckgrafico.com>
2014-12-13 12:12:09 +0100

    Add area to controller

--- MSPSpain.Web/App_Start/RouteConfig.cs
+++ MSPSpain.Web/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace MSPSpain.Web
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{area}/{controller}/{action}/{id}",
+                defaults: new { area = "MSPSpain.Web", controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
